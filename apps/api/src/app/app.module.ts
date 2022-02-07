87c820a84b613b233fe5f5dba432562b27a5452b
Sasa Fazlic <fazlic.sasa@gmail.com>
2022-02-07 10:27:08 +0100

    feat(api): setup db,config,docs and orm

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { apiConfig } from '../config/configurations'
+import { validateConfig } from '../config/validateConfig'
+import { PrismaModule } from '../prisma/prisma.module'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-    imports: [],
+    imports: [
+        PrismaModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [apiConfig],
+            validate: validateConfig
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
